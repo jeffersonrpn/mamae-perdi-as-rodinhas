e4898cb6c9e9a4e5235f490d4135b29be4fd6c05
Jefferson Neves <jefferson.rpn@gmail.com>
2016-02-03 10:05:08 -0300

    Test shake

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,13 @@ angular.module('mamae', []);
 angular.module('mamae').controller('MainController', function($scope) {
 
   $scope.showConfetti = false;
-  var popConfetti = function() {
+  $scope.popConfetti = function() {
     $scope.showConfetti = !$scope.showConfetti;
-    alert('shake');
   }
-  $scope.popConfetti = popConfetti;
 
   // Shake it off!
   var myShakeEvent = new Shake({ threshold: 15 });
   myShakeEvent.start();
-  window.addEventListener('shake', popConfetti, false);
+  window.addEventListener('shake', $scope.popConfetti, false);
 
 });
